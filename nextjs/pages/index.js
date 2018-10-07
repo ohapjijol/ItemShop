38071a0c5c9360cfdbcab711fa6c5787453943fe
jwoon <HW100@HW100-PC>
2018-10-07 19:13:05 +0900

    add app bar

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -11,11 +11,15 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Link from 'next/link';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = theme => ({
   root: {
     textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
+//    paddingTop: theme.spacing.unit * 20,
   },
 });
 
@@ -42,6 +46,17 @@ class Index extends React.Component {
 
     return (
       <div className={classes.root}>
+      	<AppBar position="static">
+	      <Toolbar>
+	        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+	          <MenuIcon />
+	        </IconButton>
+	        <Typography variant="title" color="inherit" className={classes.grow}>
+	          News
+	        </Typography>
+	        <Button color="inherit">Login</Button>
+	      </Toolbar>
+	    </AppBar>
         <Dialog open={open} onClose={this.handleClose}>
           <DialogTitle>Super Secret Password</DialogTitle>
           <DialogContent>
